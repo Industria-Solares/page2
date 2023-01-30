1ad7ffd3f0bf7e00ddff3e6fc6a01154f7519333
Bartosz 'b4rti' Gorol <bartgorol@t-online.de>
2023-01-30 20:37:13 +0000

    Text

--- pages/calculator.vue
+++ pages/calculator.vue
@@ -1,4 +1,10 @@
 <template>
+  <h1>Kalkulator</h1>
+  <p>
+    Unser Solar-Effizienzkalkulator ermöglicht es Ihnen, die Leistung Ihrer Anlage anhand Ihrer zu Verfügung stehenden Grundfläche, Modulgröße, Dachausrichtung und Ihres Standorts zu berechnen.
+    Geben Sie Ihre individuellen Daten ein, um die erwartete jährliche Energieproduktion und die damit verbundenen Einsparungen durch Solarenergie zu erfahren.
+    Weiterhin können Sie so auch Ihre mögliche Co2-Einsparung einsehen.
+  </p>
   <Calculator />
 </template>
 
@@ -6,3 +12,15 @@
 <script lang="ts" setup>
 
 </script>
+
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin: 0 0 1em 0;
+}
+p {
+  text-align: justify;
+  margin: 0 0 2em 0;
+}
+</style>
