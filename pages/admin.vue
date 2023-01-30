51509c28f75c489a8b5bbc25433b2c981db484c2
Bartosz 'b4rti' Gorol <bartgorol@t-online.de>
2023-01-30 16:03:11 +0000

    Admin panel

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,11 +1,27 @@
 <template>
-  <h1>
-    Admin
-  </h1>
-
+  <v-expansion-panels>
+    <v-expansion-panel v-for="contact in contactsData" :key="contact.id">
+      <v-expansion-panel-title>
+        <v-icon icon="mdi-email" /> {{ contact.email }}
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-icon icon="mdi-account" /> {{ contact.first_name }} {{ contact.last_name }}
+        <v-list>
+          <v-list-item>
+          </v-list-item>
+        </v-list>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 
 <script lang="ts" setup>
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://eefncycjrylkbalioskc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlZm5jeWNqcnlsa2JhbGlvc2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ3NjkwNTIsImV4cCI6MTk5MDM0NTA1Mn0.KiWwytF2Z5ipkI-xPMV2D4iGte6xdJxyF8YSQqIVWGQ')
+
+const { data: contactsData, error: contactsError } = await supabase
+  .from('contacts')
+  .select('*')
 
 </script>
